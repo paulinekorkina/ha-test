467fb2cc900a0d4f62ab7fd8bb90e79c9f89778a
Pauline Korkina <p.v.korkina@gmail.com>
2017-04-17 15:10:45 +0500

    Добавила окно подтверждения отправки

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
 var feedbackBtn = document.querySelector(".modal-label");
 var feedbackPopup = document.querySelector(".modal-feedback");
+var sendBtnList = document.querySelectorAll(".btn");
+var confirmPopup = document.querySelector(".modal-confirmation");
+var closeBtn = confirmPopup.querySelector(".confirm-close");
+var overlay = document.querySelector(".modal-overlay");
 
 feedbackBtn.addEventListener("click", function(event) {
   event.preventDefault();
   feedbackPopup.classList.toggle("modal-feedback-open");
+});
+
+for (var i = 0; i < sendBtnList.length; ++i) {
+  var sendBtn = sendBtnList[i];
+  sendBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    confirmPopup.classList.add("modal-confirmation-open");
+    overlay.classList.add("modal-overlay-open");
+  });
+}
+
+closeBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  confirmPopup.classList.remove("modal-confirmation-open");
+  overlay.classList.remove("modal-overlay-open");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (confirmPopup.classList.contains("modal-confirmation-open")) {
+      confirmPopup.classList.remove("modal-confirmation-open");
+      overlay.classList.remove("modal-overlay-open");
+    }
+  }
+});
+
+overlay.addEventListener("click", function(event) {
+  confirmPopup.classList.remove("modal-confirmation-open");
+  overlay.classList.remove("modal-overlay-open");
 });
